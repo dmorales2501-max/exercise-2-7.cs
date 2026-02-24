df85ec51dc45967234eafa7d8279736971b69e4a
dmorales2501-max <dmorales2501@student.stcc.edu>
2026-02-24 10:03:47 -0500

    Create exercise2-7.cs

---
+++ exercise2-7.cs
@@ -0,0 +1,19 @@
+// HoursAndMinutes.cs
+
+using System;
+using static System.Console;
+using System.Globalization;
+
+class HoursAndMinutes
+(
+  static void Main()
+  (
+   int minutes = 197;
+   int hours = minutes / 60;
+   int minsleft =  minutes % 60;
+    WriteLine( minutes + * minutes is * + hours * hours and * + minsleft + * ainutes.");
+
+   )
+
+)
+  
